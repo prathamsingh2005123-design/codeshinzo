a50fc50fac5f3f50cb43f2ff7194824998645eaa
Pratham <prathamsingh2005123@gmail.com>
2026-04-29 15:49:12 +0530

    fix: remove duplicate router and controller declarations

--- Backend/src/routes/userAuth.js
+++ Backend/src/routes/userAuth.js
@@ -42,4 +42,42 @@ authRouter.get("/check-auth",userMiddleware,(req,res)=>{
 
 
 
+module.exports = authRouter;const express = require('express');
+const authRouter = express.Router();
+
+const { register, login, logout, adminRegister } = require('../controllers/userAuthent');
+const userMiddleware = require('../middleware/userMiddleware');
+const adminmiddleware = require('../middleware/adminmiddleware');
+
+const userController = require('../controllers/userAuthent.js');
+
+console.log("deleteProfile check:", typeof userController.deleteProfile);
+
+// register
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
+authRouter.post("/admin/register", adminmiddleware, adminRegister);
+
+// delete profile (ONLY ONCE)
+authRouter.delete(
+  "/deleteprofile",
+  userMiddleware,
+  userController.deleteProfile
+);
+
+authRouter.get("/check-auth", userMiddleware, (req, res) => {
+    const reply = {
+        firstName: req.user.firstName,
+        email: req.user.email,
+        _id: req.user._id,
+        role: req.user.role
+    };
+
+    res.status(200).json({
+        reply,
+        message: "User is authenticated"
+    });
+});
+
 module.exports = authRouter;
