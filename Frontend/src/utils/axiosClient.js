a764ad6306e3397a98817a404cf3a935b074719e
Pratham <prathamsingh2005123@gmail.com>
2026-04-29 14:04:20 +0530

    fix: remove localhost and fix env + cors

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,9 +1,7 @@
-// Filename: Frontend/src/utils/axiosClient.js
-// this is axiosClient.js - a configured axios instance for making API calls to the backend, with base URL and credentials set up
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api', // 🔥 direct backend
+    baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
@@ -11,4 +9,3 @@ const axiosClient = axios.create({
 });
 
 export default axiosClient;
-
