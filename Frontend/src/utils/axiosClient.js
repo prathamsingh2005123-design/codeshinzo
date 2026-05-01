ed0b4f3f9789f508ed8debf63689c5bf4956b62e
Pratham <prathamsingh2005123@gmail.com>
2026-05-01 15:55:49 +0530

    fix: axios interceptor

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "https://codeshinzo-backend.onrender.com/api",
-  withCredentials: true,
 });
 
-// 🔥 REQUEST INTERCEPTOR (MAIN FIX)
+// 🔥 TOKEN ATTACH
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
+  console.log("TOKEN FROM LS =", token); // 🔍 debug
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
